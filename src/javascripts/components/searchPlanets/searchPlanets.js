4f4b944f3842425dc87f6285509acc17cd045f07
Ashley Claiborne <languageisaliquid@gmail.com>
2019-10-15 18:16:08 -0500

    started search func

--- src/javascripts/components/searchPlanets/searchPlanets.js
+++ src/javascripts/components/searchPlanets/searchPlanets.js
@@ -1,5 +1,27 @@
 import './searchPlanets.scss';
+import $ from 'jquery';
+import planets from '../../helpers/data/planets';
 import utilities from '../../helpers/utilities';
+// import planetCards from '../planetCards/planetCards';
+
+const filterPlanetList = () => {
+  const planetArr = planets.getPlanets();
+  const searchTerm = $('#searchTerm').val().toLowerCase();
+  const results = planetArr.filter((p) => p.name.toLowerCase().indexOf(searchTerm) !== -1 || p.description.toLowerCase().indexOf(searchTerm) !== -1
+  || p.nameOfLargestMoon.toLowerCase().indexOf(searchTerm) !== -1);
+  console.log(searchTerm, results);
+  //   const searchType = $('#planetType').val();
+//   for (let i = 0; i < planetArr.length; i += 1) {
+//     if ($(`${planetArr[i].name}:contains('${searchTerm}')`)) {
+//       console.log(searchTerm, planetArr[i].name);
+//     //   planetCards.printPlanetCards();
+//     }
+//   }
+};
+
+const searchClick = () => {
+  $('#filterPlanets').click(filterPlanetList);
+};
 
 const printSearchForm = () => {
   const searchString = `
@@ -20,6 +42,7 @@ const printSearchForm = () => {
   </form>
   `;
   utilities.printToDom('searchBar', searchString);
+  searchClick();
 };
 
 export default { printSearchForm };
